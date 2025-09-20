d4edf173da681863c35528483f5ab161b43a4f4c
72897 <115211846+72897@users.noreply.github.com>
2025-09-20 22:54:56 +0530

    Update GuestDownload.jsx

--- client/src/components/Guest/Download/GuestDownload.jsx
+++ client/src/components/Guest/Download/GuestDownload.jsx
@@ -16,7 +16,7 @@ const GuestDownload = () => {
 
   const fetchFile = async () => {
     try {
-      const res = await fetch(`pastebox-file-sharing-platform-main.railway.internal/api/files/g/${shortCode}`, {
+      const res = await fetch(`pastebox-file-sharing-platform-main-production.up.railway.app/api/files/g/${shortCode}`, {
         signal: controller.signal,
       });
 
@@ -64,7 +64,7 @@ const GuestDownload = () => {
     }
 
     try {
-      const res = await fetch(`pastebox-file-sharing-platform-main.railway.internal/api/files/verifyGuestFilePassword`, {
+      const res = await fetch(`pastebox-file-sharing-platform-main-production.up.railway.app/api/files/verifyGuestFilePassword`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ shortCode, password }),
