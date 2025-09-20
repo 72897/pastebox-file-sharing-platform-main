6e90b969ea34161fc8eebbdaebb13a428dd6a05d
72897 <115211846+72897@users.noreply.github.com>
2025-09-20 22:53:39 +0530

    Update axiosInstance.js

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="pastebox-file-sharing-platform-main.railway.internal/api/"
+const BASE_URL="pastebox-file-sharing-platform-main-production.up.railway.app/api/"
 const axiosInstance=axios.create()
 
 axiosInstance.defaults.baseURL=BASE_URL;
